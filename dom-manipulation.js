c52bf2f580e1381f3f2379e256d590d393f52328
Amneris Calle <amneris.calle@gmail.com>
2024-10-12 12:32:35 -0300

    [SOLVED] se coloca un caracter en lugar de un div circular

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,15 +1,5 @@
 const colores = ["yellow", "blue", "red", "white", "green", "brown", "violet"];
-const style = document.querySelector("style"); // Create a style tag
-style.innerHTML += `
-.flotas {
-  width:8px;
-  height:8px;
-  border-radius:50%;
-  display:inline-block;
-  margin-left: 5px;
-}
-`;
-document.head.appendChild(style); // Append style tag to the head
+
 const rows = document.querySelectorAll("tr.summaryrow");
 console.log("cantidad de filas: ", rows.length);
 rows.forEach((value, key) => (value.id = `puesto-${key + 1}`));
@@ -45,7 +35,6 @@ rows.forEach((row, key) => {
   querySel[key] = document.querySelectorAll(`tr#puesto-${key + 1}>td`);
 });
 
-console.log("query sel luego del forEach", querySel);
 for (let j = 0; j < rows.length; j++) {
   celdas[j] = [...querySel[j]].slice(
     primeraColumna,
@@ -60,14 +49,12 @@ for (let j = 0; j < rows.length; j++) {
     if (celdas[j][i].innerHTML == "&nbsp;") {
       console.log("celda en posicion ", i, "en fila ", j, " ", celdas[j][i]);
 
-      /* celdas[j][i].parentNode.removeChild(celdas[j][i]); */
       celdas[j][i].classList.add("esconder");
     } else {
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("flotas");
-      newDiv.style.backgroundColor = colores[v];
+      const newDiv = document.createElement("span");
+      newDiv.style.color = colores[v];
+      newDiv.innerHTML = "&nbsp; &#9830;";
       celdas[j][i].appendChild(newDiv);
-      /* celdas[j][i].style.backgroundColor = colores[v]; */
     }
     v++;
   }
