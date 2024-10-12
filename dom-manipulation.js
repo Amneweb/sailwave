7da4422b531f2d7d816c1fbf0ce477fc83231efe
Amneris Calle <amneris.calle@gmail.com>
2024-10-12 10:53:00 -0300

    [UNRESOLVED] no puedo borrar celdas vacias

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,3 +1,4 @@
+const colores = ["yellow", "blue", "red", "white", "green", "brown", "violet"];
 const rows = document.querySelectorAll("tr.summaryrow");
 console.log("cantidad de filas: ", rows.length);
 rows.forEach((value, key) => (value.id = `puesto-${key + 1}`));
@@ -10,7 +11,7 @@ primeraColumna = [...cols].findIndex((col) => col.classList.contains("race"));
 console.log("primera Columna ", primeraColumna);
 
 const totalColumnasResultados = document.querySelectorAll(".race").length;
-console.log("total columnas de resultados ", totalColumnasResultados);
+
 const encabezados = document.querySelectorAll("tr.titlerow > th");
 
 const regata = encabezados[primeraColumna].innerText;
@@ -22,25 +23,54 @@ while (i < columnas && encabezados[i + 1].innerText === regata) {
   console.log("i", i);
   flotas++;
 }
+const regatasCorridas = totalColumnasResultados / flotas;
+console.log("total columnas de resultados ", totalColumnasResultados);
+console.log("regatas corridas ", regatasCorridas);
+console.log("flotas", flotas);
+let celdas = [];
 let querySel = [];
-console.log(flotas);
-let celdas;
+let j = 0;
 rows.forEach((row, key) => {
-  querySel[key] = document.querySelectorAll(`tr#puesto-${key}>td`);
-  console.log("celdas query");
-  console.log(querySel[key]);
-  console.log([...querySel[key]]);
-  celdas[key] = [...querySel[key]].slice(
+  querySel[key] = document.querySelectorAll(`tr#puesto-${key + 1}>td`);
+});
+
+console.log("query sel luego del forEach", querySel);
+for (let j = 0; j < rows.length; j++) {
+  celdas[j] = [...querySel[j]].slice(
     primeraColumna,
     primeraColumna + totalColumnasResultados
   );
-  console.log("celdas ");
-  console.log(document.querySelectorAll(`tr#puesto-${key}>td`));
-  /* for (let j = 0; j < flotas; j++) {
-    if (celdas[j].innerHTML == "&nbsp;") {
-      celdas[j].style.display = "none";
-    } else {
-      celdas[j].style.backgroundColor = "red";
+  console.log("celdas en fila ", j, celdas[j]);
+  let v = 0;
+  for (let i = 0; i < totalColumnasResultados; i++) {
+    if (i % flotas === 0) {
+      v = 0;
     }
-  } */
-});
+    if (celdas[j][i].innerHTML == "&nbsp;") {
+      console.log("celda en posicion ", i, "en fila ", j, " ", celdas[j][i]);
+      celdas[j][i].style.display = "none";
+      querySel[j];
+      /* celdas[j][i].classList.add("esconder"); */
+    } else {
+      celdas[j][i].style.backgroundColor = colores[v];
+    }
+    v++;
+  }
+}
+const encabezadosRegatas = [...cols].slice(
+  primeraColumna,
+  primeraColumna + totalColumnasResultados
+);
+console.log(encabezadosRegatas);
+/* let v = 0;
+encabezadosRegatas.forEach((nodo, key) => {
+  if (key % flotas === 0 && key < primeraColumna + totalColumnasResultados) {
+    v = 0;
+    nodo.classList.add("esconder");
+  } else {
+    if (v < flotas - 1) nodo.classList.add("esconder");
+  }
+  v++;
+}); 
+const escondidos = document.querySelectorAll(".esconder");
+escondidos.forEach((elemento) => (elemento.style.display = "none"));*/
